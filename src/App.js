3c479d53f5737f37374725fb9db4ec53e5e04617
Bentax <bentaxrus@mail.ru>
2023-06-02 17:29:40 +0300

    new contract

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-            <h1 className="navbar-item is-size-4">Ocean Token (OCT)</h1>
+            <h1 className="navbar-item is-size-4">Antlaws DAO Token (ADAO)</h1>
           </div>
           <div id="navbarMenu" className="navbar-menu">
             <div className="navbar-end is-align-items-center">
@@ -124,7 +124,7 @@ function App() {
         <div className="faucet-hero-body">
           <div className="container has-text-centered main-content">
             <h1 className="title is-1">Faucet</h1>
-            <p>Fast and reliable. 50 OCT/day.</p>
+            <p>Fast and reliable. 1 ADAO/hour.</p>
             <div className="mt-5">
               {withdrawError && (
                 <div className="withdraw-error">{withdrawError}</div>
--- src/ethereum/faucet.js
+++ src/ethereum/faucet.js
@@ -4,19 +4,6 @@ const faucetAbi = [
 	{
 		"inputs": [
 			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "requestTokens",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
 				"internalType": "address",
 				"name": "_tokenAddress",
 				"type": "address"
@@ -31,7 +18,7 @@ const faucetAbi = [
 			{
 				"indexed": true,
 				"internalType": "address",
-				"name": "user",
+				"name": "from",
 				"type": "address"
 			},
 			{
@@ -41,18 +28,62 @@ const faucetAbi = [
 				"type": "uint256"
 			}
 		],
-		"name": "TokensSent",
+		"name": "Deposit",
 		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "requestTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
 			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
 			}
 		],
-		"name": "lastRequestTime",
+		"name": "setWithdrawalAmount",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdrawal",
+		"type": "event"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -75,10 +106,24 @@ const faucetAbi = [
 		],
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawalAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	}
 ];
+
 const faucetContract = (provider) => {
-    return new ethers.Contract("0x677ad79e081445985885a109d6b59973803dbd4f", faucetAbi, provider);
+    return new ethers.Contract("0x8fc2fd51d59be938fa1cccc39daa15d4938a8ae3", faucetAbi, provider);
 
 };
 
