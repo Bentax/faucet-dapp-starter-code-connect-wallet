cd386757a9553debda8e6492f5330ec69c91d9b2
Bentax <bentaxrus@mail.ru>
2023-06-02 09:56:06 +0300

    Initial commit

---
+++ src/ethereum/faucet.js
@@ -0,0 +1,9 @@
+import {ethers} from 'ethers';
+
+const faucetAbi = [];
+const faucetContract = (provider) => {
+    return new ethers.Contract("", faucetAbi, provider);
+
+};
+
+export default faucetContract;
